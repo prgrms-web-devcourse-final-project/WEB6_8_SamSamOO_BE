215bea3c5110f89ae4d6609bc28b6097a2640587
yongho9064 <dydgmldydgh@naver.com>
2025-09-25 09:24:13 +0900

    feat[repository]: 법령 엔티티 기반 조회 메서드 추가

--- backend/src/main/java/com/ai/lawyer/domain/law/repository/HoRepository.java
+++ backend/src/main/java/com/ai/lawyer/domain/law/repository/HoRepository.java
@@ -1,9 +1,13 @@
 package com.ai.lawyer.domain.law.repository;
 
+import com.ai.lawyer.domain.law.entity.Hang;
 import com.ai.lawyer.domain.law.entity.Ho;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface HoRepository extends JpaRepository<Ho, Long> {
+    List<Ho> findByHang(Hang hang);
 }
