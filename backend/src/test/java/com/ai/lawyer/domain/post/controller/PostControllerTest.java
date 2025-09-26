ad6afc69cedc10032137b0f20b918dd073d6ef79
yongho9064 <dydgmldydgh@naver.com>
2025-09-26 18:44:25 +0900

    fix:[ci/cd]: 버그 수정

--- backend/src/test/java/com/ai/lawyer/domain/post/controller/PostControllerTest.java
+++ backend/src/test/java/com/ai/lawyer/domain/post/controller/PostControllerTest.java
@@ -1,7 +1,6 @@
 package com.ai.lawyer.domain.post.controller;
 
 import com.ai.lawyer.domain.post.dto.PostDto;
-import com.ai.lawyer.domain.post.entity.Post;
 import com.ai.lawyer.domain.post.service.PostService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -9,9 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -19,13 +16,12 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ActiveProfiles("test")
-@SpringBootTest
+//@SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
