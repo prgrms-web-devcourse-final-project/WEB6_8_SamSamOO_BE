b8fd3a3e392f34bbf114127b49fa2e528623fa74
asowjdan <asowjdan@naver.com>
2025-09-26 10:57:13 +0900

    fix[member]: 비밀번호 재설정 인증번호 체크여부 검증 로직 추가

--- backend/src/main/java/com/ai/lawyer/domain/member/dto/ResetPasswordRequestDto.java
+++ backend/src/main/java/com/ai/lawyer/domain/member/dto/ResetPasswordRequestDto.java
@@ -8,7 +8,6 @@ import lombok.Setter;
 @Getter
 @Setter
 public class ResetPasswordRequestDto {
-    @NotBlank(message = "이메일을 입력해주세요.")
     private String loginId;
 
     @NotBlank(message = "새 비밀번호를 입력해주세요.")
