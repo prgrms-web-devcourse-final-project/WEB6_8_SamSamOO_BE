be55f8ec2c1fef290dadf7a3a2a864b6800e405f
yongho9064 <dydgmldydgh@naver.com>
2025-09-27 18:03:30 +0900

    chore[chat]: chatMemory 주석 해제 및 도커 설정

--- backend/src/main/java/com/ai/lawyer/domain/chatbot/entity/ChatMemory.java
+++ backend/src/main/java/com/ai/lawyer/domain/chatbot/entity/ChatMemory.java
@@ -1,6 +1,11 @@
 package com.ai.lawyer.domain.chatbot.entity;
 
-/*
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.ai.chat.messages.MessageType;
+
 @Entity
 @Data
 @NoArgsConstructor
@@ -19,4 +24,4 @@ public class ChatMemory {
     @Column(name = "type", nullable = false)
     private MessageType type;
 
-}*/
+}
