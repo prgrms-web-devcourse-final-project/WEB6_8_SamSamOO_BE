45ad00bf07c093a4b0fde295ecf06d464c72a375
yongho9064 <dydgmldydgh@naver.com>
2025-10-02 22:36:08 +0900

    ci/cd: 수정

--- backend/src/main/java/com/ai/lawyer/global/config/MetaDBConfig.java
+++ backend/src/main/java/com/ai/lawyer/global/config/MetaDBConfig.java
@@ -1,7 +1,6 @@
 package com.ai.lawyer.global.config;
 
 import org.springframework.boot.autoconfigure.batch.BatchDataSource;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
@@ -11,7 +10,7 @@ import org.springframework.context.annotation.Profile;
 import javax.sql.DataSource;
 
 @Configuration
-@Profile("!test")
+@Profile("!test-ci")
 public class MetaDBConfig {
 
     @Bean
