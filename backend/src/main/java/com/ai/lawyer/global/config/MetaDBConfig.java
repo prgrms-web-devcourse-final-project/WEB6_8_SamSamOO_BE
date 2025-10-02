5a717ce454aafffdef5cc17dd9fdbaceeb5b4aa5
yongho9064 <dydgmldydgh@naver.com>
2025-10-02 23:46:00 +0900

    chore[db]: @Profile 추가

--- backend/src/main/java/com/ai/lawyer/global/config/MetaDBConfig.java
+++ backend/src/main/java/com/ai/lawyer/global/config/MetaDBConfig.java
@@ -5,9 +5,11 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import javax.sql.DataSource;
 
+@Profile("!test & !test-ci")
 @Configuration
 public class MetaDBConfig {
 
