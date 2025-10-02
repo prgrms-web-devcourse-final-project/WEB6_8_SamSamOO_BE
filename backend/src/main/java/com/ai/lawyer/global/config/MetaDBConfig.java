977a166ce6bf62b249503b506bd5229831c64135
yongho9064 <dydgmldydgh@naver.com>
2025-10-02 23:20:12 +0900

    ci/cd: 수정

--- backend/src/main/java/com/ai/lawyer/global/config/MetaDBConfig.java
+++ backend/src/main/java/com/ai/lawyer/global/config/MetaDBConfig.java
@@ -5,12 +5,10 @@ import org.springframework.boot.autoconfigure.batch.BatchDataSource;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 import javax.sql.DataSource;
 
 @Configuration
-@Profile("!test-ci")
 public class MetaDBConfig {
 
     @Value("${spring.datasource-meta.url}")
