9bd81c6fb2303a2cc560a1d891fa4e35763d2816
yongho9064 <dydgmldydgh@naver.com>
2025-10-02 22:22:15 +0900

    ci/cd: 수정

--- backend/src/main/java/com/ai/lawyer/global/config/DataDBConfig.java
+++ backend/src/main/java/com/ai/lawyer/global/config/DataDBConfig.java
@@ -21,7 +21,6 @@ import java.util.HashMap;
         entityManagerFactoryRef = "dataEntityManager",
         transactionManagerRef = "dataTransactionManager"
 )
-@Profile("!test")
 public class DataDBConfig {
 
     @Value("${spring.datasource.url}")
