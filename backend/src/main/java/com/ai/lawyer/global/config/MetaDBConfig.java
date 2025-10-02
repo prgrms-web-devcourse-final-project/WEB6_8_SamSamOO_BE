7fa80f311714802439c869fc00ef7cf67bbb95af
yongho9064 <dydgmldydgh@naver.com>
2025-10-02 23:09:33 +0900

    ci/cd: 수정

--- backend/src/main/java/com/ai/lawyer/global/config/MetaDBConfig.java
+++ backend/src/main/java/com/ai/lawyer/global/config/MetaDBConfig.java
@@ -1,24 +1,37 @@
 package com.ai.lawyer.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.batch.BatchDataSource;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 import javax.sql.DataSource;
 
-/*
 @Configuration
-@Profile("!test")
 public class MetaDBConfig {
+
+    @Value("${spring.datasource-meta.jdbc-url}")
+    private String url;
+
+    @Value("${spring.datasource-meta.username}")
+    private String username;
+
+    @Value("${spring.datasource-meta.password}")
+    private String password;
+
+    @Value("${spring.datasource-meta.driver-class-name}")
+    private String driver;
 
     @Bean
     @BatchDataSource
-    @ConfigurationProperties(prefix = "spring.datasource-meta")
     public DataSource metaDBSource() {
-        return DataSourceBuilder.create().build();
+        return DataSourceBuilder.create()
+                .url(url)
+                .username(username)
+                .password(password)
+                .driverClassName(driver)
+                .build();
     }
 
-}*/
+}
